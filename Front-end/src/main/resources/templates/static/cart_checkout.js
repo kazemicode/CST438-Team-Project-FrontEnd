8f605b5677def622cfeaee0d1cf116fc26f615b0
Cody Young <cyoung1623@gmail.com>
2020-04-09 20:58:47 -0700

    Added Javascript, updated HTML

---
+++ Front-end/src/main/resources/templates/static/cart_checkout.js
@@ -0,0 +1,40 @@
+// Variables
+var tip = 0.00;
+var subtotal = 0.00;
+var finalCost = 0.00;
+
+//To do: Calculate subtotal of all order line items
+function calcSubTotal() {
+    document.getElementById("subtotal").innerHTML = "subtotal: $" + subtotal;
+}
+
+//Returns final total - subtotal + tip
+function calcFinal(subCost) {
+    finalCost = parseInt(finalCost);
+    finalCost = parseFloat(tip + subtotal);
+    finalCost = finalCost.toFixed(2);
+    document.getElementById("total").innerHTML = "total: $" + finalCost;
+}
+
+// jQuery Handlers
+$(".checkoutButton").on("click", function(){
+    var timestamp = new Date($.now());
+    
+    if (finalCost > 0) {
+        $("#order_confirm_msg").html('Your order has been confirmed ' + '<br />' + timestamp + '');
+        $("#order_confirm_msg").show();
+
+        // Disable cart update and checkout buttons after order confirmation
+        $(".startOver").attr("disabled", true);
+        $(".checkoutButton").attr("disabled", true);
+    }
+    else {
+        $("#order_confirm_msg").html('Error: Empty cart');
+        $("#order_confirm_msg").show();
+    }
+});
+
+// If "Start Over" is clicked, goes back to the beginning
+$(".startOver").on("click", function(){
+    // Calls function to start over
+});
