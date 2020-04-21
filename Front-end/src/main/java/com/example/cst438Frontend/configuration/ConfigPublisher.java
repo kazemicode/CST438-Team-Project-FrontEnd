0c65496cfd8e79f84125a06e99f7aed0522400a1
Nathan Warren-Acord <nacord26@gmail.com>
2020-04-21 13:22:41 -0700

    Added primary annotation for order messenger.

--- Front-end/src/main/java/com/example/cst438Frontend/configuration/ConfigPublisher.java
+++ Front-end/src/main/java/com/example/cst438Frontend/configuration/ConfigPublisher.java
@@ -3,6 +3,7 @@ import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /* Sends Message to receiver (separate backend service) */
 
@@ -11,6 +12,7 @@ public class ConfigPublisher {
 	
 	@Bean
 	@Qualifier("fanoutOrder")
+	@Primary
 	public FanoutExchange fanoutOrder() {
 		return new FanoutExchange("restaurant-order");
 	}
