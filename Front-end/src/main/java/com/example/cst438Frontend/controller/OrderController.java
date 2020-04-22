ef64ff6df858e0dcfa7874f7dfaf846438ef8758
kazemicode <sarieses@gmail.com>
2020-04-22 00:55:09 -0700

    show complete delivery address in delivery message

--- Front-end/src/main/java/com/example/cst438Frontend/controller/OrderController.java
+++ Front-end/src/main/java/com/example/cst438Frontend/controller/OrderController.java
@@ -224,7 +224,7 @@ public class OrderController {
 		customerRepository.save(customer); // save customer, should cascade to saving order/orderlineitems
 		List<LineItemInfo> lineInfos = orderService.getLineItemInfo(order.getOrderLineItems());
 		orderService.requestOrder(order.getId(), order.getOrderDatetime().toString(), customer.getFirstName() + " " + customer.getLastName(), customer.getPhone(), customer.getAddress1(), lineInfos, order.getOrderTotal(), paymentType);
-		orderService.requestDelivery(order.getId(), order.getOrderDatetime().toString(), customer.getAddress1(), lineInfos.get(0).getRestId());
+		orderService.requestDelivery(order.getId(), order.getOrderDatetime().toString(), customer.getAddress1() + " " + customer.getAddress2() + " " +  customer.getCity() + " " + customer.getState() + " " + customer.getZipcode(), lineInfos.get(0).getRestId());
 		sessionRepository.deleteById(session.getId());
 		model.addAttribute("orderId", order.getId());
 		return "order_success";
